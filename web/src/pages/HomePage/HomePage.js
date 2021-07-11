a59d186bc0fcd3ad7a2c2308077af6a966631a13
memarino92 <memarino92@gmail.com>
2021-07-11 00:26:48 -0400

    added about page and link from home

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,23 @@
+// web/src/pages/HomePage/HomePage.js
+
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
