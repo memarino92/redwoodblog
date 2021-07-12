caa71c8095e618bfd410f1799eacdd9b61eea2e1
memarino92 <memarino92@gmail.com>
2021-07-12 00:22:10 -0400

    upgraded to redwood 0.35.0

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -4,11 +4,14 @@ import {
   makeServices,
 } from '@redwoodjs/api'
 
+import { logger } from 'src/lib/logger'
+
 import schemas from 'src/graphql/**/*.{js,ts}'
 import { db } from 'src/lib/db'
 import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
+  loggerConfig: { logger, options: {} },
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
